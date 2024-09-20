d454236ceea76a5c309f4d0adb92350ffdff1d61
MusokiLillian <musokilillian601@gmail.com>
2024-09-20 18:05:05 +0300

    Musoki Lillian.php

---
+++ Musoki Lillian.php
@@ -0,0 +1,16 @@
+<?php 
+$Name = "MUSOKI LILLIAN";
+$DOB = "06/09/2001";
+$Address = "KIHALIMU-KASESE";
+$current_year = 2024;
+$year_of_birth = 2001;
+$age = $current_year - $year_of_birth;
+$welcome = "welcome back home";
+
+echo "Name: " . $Name . "\n";
+echo "Date of birth: " . $DOB . "\n";
+echo "Home Address: " . $Address . "\n";
+echo "Welcome! miss Lillian, you are now " . $age . " years old.\n";
+echo "you are" . $welcome . "\n";
+
+?>
